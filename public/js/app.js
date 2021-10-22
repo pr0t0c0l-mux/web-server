1cdadaa480b9f217c71b5787d5861de0773caf3f
Leader Troy <sync132619@gmail.com>
2021-10-22 22:42:59 +0800

    search element

--- public/js/app.js
+++ public/js/app.js
@@ -14,8 +14,12 @@ fetch('http://localhost:3000/weather?address=!'). then ((response) => {
 })
 
 const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('testing!')
+
+    const location = search.value
+
+    console.log(location)
 })
