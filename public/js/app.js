68d4c2063aa8b52c96cb57e3a6bb399bf13ef827
Leader Troy <sync132619@gmail.com>
2021-10-22 21:33:13 +0800

    creating fetch weather address

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
 console.log('Client side javascript file is loaded.')
 
-fetch('')
+fetch('http://localhost:3000/weather?address=!'). then ((response) => {
+    response.json().then((data) => {
+        console.log(data)
+        if (data.error) {
+            console.log(data.error)
+
+        }else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
